525018e292c31ba4eb1dc937deb071da85ed6156
tyler <tyler.william.palmer@gmail.com>
2019-09-11 22:30:04 -0600

    fixed paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,11 @@
 import React, { Component } from 'react'
-import BlogContainer from './Container/Home/Home'
-import Strapi from 'strapi-sdk-javascript'
-const strapi = new Strapi('http://localhost:1337')
+import Home from './Components/Container/Home/Home'
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            posts: []
-        }
-    }
-    async componentDidMount() {
-        try {
-            const posts = await strapi.getEntries('blogposts')
-            this.setState({ posts })
-        } catch (err) {
-            console.log(err)
-        }
-        console.log(this.state.posts)
-    }
 
     render() {
         return (
-            
+            <Home />
         )
     }
 }
