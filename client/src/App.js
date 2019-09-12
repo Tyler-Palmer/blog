1a5ec6568db7ac086363253d29c2ec363f97da93
tyler <tyler.william.palmer@gmail.com>
2019-09-11 22:57:06 -0600

    building out structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
 import Home from './Components/Container/Home/Home'
+import Navbar from './Components/Container/Navbar/Navbar'
+import About from './Components/Container/About/About'
+import Footer from './Components/Container/Footer/Footer'
 
 class App extends Component {
-
     render() {
         return (
-            <Home />
+            <div>
+                <Navbar />
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route exact path='/about' component={About} />
+                </Switch>
+                <Footer />
+            </div>
         )
     }
 }
